e3575542f168168e096c5a92a0e1efd3da0e8cea
Gray <gray.norton@lge.com>
2015-06-23 15:12:42 -0700

    Updating to second-gen enyo-dev tools (from unbrowserify branch)
    Enyo-DCO-1.1-Signed-Off-By: Gray Norton (gray.norton@lge.com)

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var
-	Packager = require('./lib/Packager');
+	Packager = require('./lib/Packager'),
+	Watcher = require('./lib/Watcher');
 
 /**
 * Package an Enyo 2.6+ application based on the provided options.
 */
 exports.package = function (opts) {
-	return new Packager(opts);
+	opts = opts || {};
+	if (opts.watch) return new Watcher(opts);
+	else return new Packager(opts).run();
 };
